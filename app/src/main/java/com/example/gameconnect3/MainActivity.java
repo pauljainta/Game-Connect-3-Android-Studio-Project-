ec7b8ea2d0ed3de7dce089ad2e947313c0134430
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-09 20:37:30 +0600

    Checking Winning Positions Left

--- app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -10,10 +10,20 @@ public class MainActivity extends AppCompatActivity {
 
     int player;
 
+    // red:1,yellow:2, empty:3
+
+    int[] gameState={3,3,3,3,3,3,3,3,3};
+    int[][] winningPositions={{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}};
+
+
 
     public void play(View view)
     {
         ImageView imageView=(ImageView) view;
+
+        int tappedImage=Integer.parseInt(imageView.getTag().toString());
+
+        gameState[tappedImage]=player;
 
         if(player==1)
         {
