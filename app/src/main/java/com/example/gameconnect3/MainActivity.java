43330deef83484aaeb3627be0f2bcb1267548ab1
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-09 23:43:00 +0600

    Checking Winning Positions Left

--- app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.gameconnect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,34 @@ public class MainActivity extends AppCompatActivity {
     int[][] winningPositions={{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}};
 
 
+    public boolean CheckWinningPositions(int P)
+    {
+        boolean hasWon=false;
+
+            for(int i=0;i<winningPositions.length;i++)
+            {
+                int counter=0;
+                for(int j=0;j<3;j++)
+                {
+                    if(gameState[winningPositions[i][j]-1]==P) counter++;
+                }
+                Log.i("COUNTER",counter+"");
+
+                if(counter==3)
+                {
+                    hasWon=true;
+                    break;
+                }
+            }
+
+
+
+
+        return hasWon;
+    }
+
+
+
 
     public void play(View view)
     {
@@ -23,16 +54,30 @@ public class MainActivity extends AppCompatActivity {
 
         int tappedImage=Integer.parseInt(imageView.getTag().toString());
 
-        gameState[tappedImage]=player;
+        gameState[tappedImage-1]=player;
 
         if(player==1)
         {
             imageView.setImageResource(R.drawable.red);
+           if(CheckWinningPositions(player))
+           {
+               //Toast.makeText(this,player+" has won")
+               Log.i("JITSE NAKI?",player+" has won");
+
+
+
+           }
             player=2;
         }
         else
         {
             imageView.setImageResource(R.drawable.yellow);
+            if(CheckWinningPositions(player))
+            {
+                Log.i("JITSE NAKI?",player+" has won");
+               return;
+            }
+
             player=1;
         }
 
